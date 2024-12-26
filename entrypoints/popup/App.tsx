0d78cd9671e05fb07e586a97d093b3265f60cc9d
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-12-26 14:21:37 +0800

    feat: github link

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -22,10 +22,21 @@ function App() {
 
   return (
     <div className="min-w-32 p-4 bg-[#fff4da]">
-      <div className="mb-4">
+      <div className="mb-4 flex justify-between items-center">
         <h1 className="text-2xl font-bold tracking-tight">
-            <span className="text-gray-900">Git</span><span className="text-[#fca847]">ingest</span>
+            <span className="text-gray-900">Git</span><span className="text-[#fca847]">ingest</span> <span className="text-gray-600 tracking-tighter">Extension</span>
         </h1>
+        <a 
+          href="https://github.com/lcandy2/gitingest-extension" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="text-gray-800"
+          title="View on GitHub"
+        >
+          <svg height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg>
+        </a>
       </div>
 
       <div className="rounded-xl relative z-20 pl-6 sm:pl-8 pr-6 sm:pr-12 py-6 border-[3px] border-gray-900 bg-[#fff4da]">
@@ -67,10 +78,6 @@ function App() {
         {isSaved && (
           <div className="mt-6 text-sm text-green-600 font-medium text-center relative z-20">
             Settings saved!
-            <br />
-            {/* <span className="ml-2 font-normal text-gray-600 text-xs">
-              You may need to refresh the page.
-            </span> */}
           </div>
         )}
       </div>
