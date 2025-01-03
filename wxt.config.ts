57dea7aedfc5e3cfd844b57c809f6ed23e978a43
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2025-01-03 11:42:12 +0800

    ci: automatic short name for firefix

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
-  manifest: {
-    name: 'Git Ingest - Turn any Git repo to a LLM-friendly prompt',
+  manifest: (env: ConfigEnv) => ({
+    name: (env.browser === 'firefox') ? 'Git Ingest - Turn any Git repo to LLM prompt' : 'Git Ingest - Turn any Git repo to a LLM-friendly prompt',
     description: 'Turn any Git repository into a prompt-friendly text ingest for LLMs. By replacing hub with ingest to access a coresponding digest.',
     permissions: ['storage'],
-  }
+  })
 });
