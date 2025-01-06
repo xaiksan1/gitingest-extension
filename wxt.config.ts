3b820b78d9a78f1616ab0a98a8a53e5c7d085c5e
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2025-01-06 10:02:57 +0800

    feat: use name GitIngest && bump version to 1.1.2

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
   manifest: (env: ConfigEnv) => ({
-    name: (env.browser === 'firefox') ? 'Git Ingest - Turn any Git repo to LLM prompt' : 'Git Ingest - Turn any Git repo to a LLM-friendly prompt',
+    name: `GitIngest${(env.browser === 'firefox') ? ' - Turn any Git repo to LLM prompt' : ' - Turn any Git repo to a LLM-friendly prompt'}`,
     description: 'Turn any Git repository into a prompt-friendly text ingest for LLMs. By replacing hub with ingest to access a coresponding digest.',
     permissions: ['storage'],
   })
